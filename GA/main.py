663137ab934a0c1e2da74dd2211e49377ff1a2e5
alexv <ugarte_alex@discente.ufg.br>
2022-09-14 17:05:07 -0300

    Melhorias feitas!

--- GA/main.py
+++ GA/main.py
@@ -1,70 +1,105 @@
 import numpy as np
 from mealpy.evolutionary_based.GA import BaseGA
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
 
-# a constante define o grau de importancia da quantidade de features na funcao fitness
-lamda = 3
-qtd_features_maximas = 4000
+def main(qtd_features_maximas):
+    clf = RandomForestClassifier(criterion='entropy', n_estimators=100)
+    def classificador(data_to_train, data_to_test, y_train, y_test):
+        # instanciando o classificador
+        clf.fit(data_to_train, y_train)
+        y_chapeu = clf.predict(data_to_test)
 
-# vamos determinar que um array binário represente as features de nosso dataset,
-# sendo 1 quando a feature estará presente e 0 quando ausente.
-# Isso elimina o problema de reptição de features, ao mesmo tempo que é simples.
-# df_train = pd.read_csv('../Dados/train.csv')
-# df_test = pd.read_csv('../Dados/test.csv')
+        return accuracy_score(y_test, y_chapeu)
+
+    def fitness(features):
+        # agora a quantidade de features sempre será fixa
+        # precisamos lidar com valores repetidos
+
+        if features.shape[0] - np.unique(features).shape[0] != 0:
+            return 10000
+        # precisamos de valores inteiros pois são sempre os indices das features a considerar
+        features = np.round(features).astype(np.int32).astype(str)
+
+        data_to_train = X_train_final.loc[:, features]
+        data_to_test = X_val_final.loc[:, features]
+
+        acuracia = classificador(data_to_train, data_to_test, y_train_final, y_val_final)
+
+        # unindo os dois objetivos em uma unica expressao, tornar um problema de minimizacao
+        return 1/acuracia
+
+    problem_dict = {
+        'fit_func': fitness,
+        'lb': 0,
+        'ub': 194999,
+        'minmax': 'min',
+        'save_population': False,
+        'n_dims': qtd_features_maximas,
+    }
+
+    epoch = 100
+    pop_size = 100
+    pc = 0.95
+    pm = 0.025
+    selection = 'roulette'
+    model1 = BaseGA(problem_dict, epoch, pop_size, pc, pm, crossover='one_point', selection=selection)
+    best_position, best_fitness = model1.solve()
+    print(f"Solution {qtd_features_maximas}: {best_position}, Fitness: {best_fitness}")
+
+    ## Salvando as features encontradas em um arquivo txt
+    with open(f"../Features/AG_features_{qtd_features_maximas}_{best_fitness:.2f}.txt", "w") as f:
+        for i in best_position:
+            f.write(str(i) + '\n')
+
+    # a partir desse ponto gera-se os dados de teste para submissao no kaggle
+    best_position.sort()
+    features_finais = np.round(best_position).astype(np.int32).astype(str)
+    val = pd.read_csv("../Dados/test.csv", dtype="float64")
+    beer_id = val.beer_id.astype(np.int32)
+    data_to_test = val.loc[:, features_finais]
+    data_to_test.replace([np.inf, -np.inf], np.nan, inplace=True)
+    data_to_test.fillna(data_to_test.mean(), inplace=True)
+
+    # classificando os dados de teste
+    # treinando nos dados
+    clf = RandomForestClassifier(criterion='entropy', n_estimators=100)
+    clf.fit(X_train_final.loc[:, features_finais], y_train_final)
+    predictions = clf.predict(data_to_test.astype("float32"))
+    submission = pd.DataFrame(np.stack((beer_id, predictions), axis=1),
+                              columns=["beer_id", "target"]).astype(dtype={"beer_id":"int32", "target":"float64"})
+    submission.to_csv(f"../Submissions/Submission{qtd_features_maximas}.csv", index=False)
 
 
+if __name__ == '__main__':
+    import threading
+    import logging
 
-# após o pré-processamento, essas devem ser as variaveis que recebem os dados
-X_train_final = None
-y_train_final = None
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
 
-X_val_final = None
-y_val_final = None
+    # após o pré-processamento, essas devem ser as variaveis que recebem os dados
+    X_train_final = pd.read_csv('../Dados/DataTrain.csv', dtype="float32")
+    y_train_final = X_train_final['target']
 
-colunas = df_train.drop(columns=['beer_id', 'target']).columns
+    X_val_final = pd.read_csv('../Dados/DataVal.csv', dtype="float32")
+    y_val_final = X_val_final['target']
 
-def classificador(data_to_train, data_to_test, y_train, y_test):
-    # instanciando o classificador
-    clf = LogisticRegression(solver='liblinear')
-    clf.fit(data_to_train, y_train)
-    y_chapeu = clf.predict(data_to_test)
+    colunas = X_train_final.columns
 
-    return accuracy_score(y_test, y_chapeu)
+    qtd_features = [301, 302, 303, 299]
 
+    threads = list()
+    for index in range(len(qtd_features)):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=main, args=(qtd_features[index],))
+        threads.append(x)
+        x.start()
 
-def fitness(features):
-    qtd_features = features.sum()  # as features entram como vetor binario, por isso .sum()
-
-    # se a quantidade de features for maior que a quantidade máxima, retorna um valor altíssimo
-    if qtd_features > qtd_features_maximas:
-        return 999999
-
-    # transformando o vetor binario para booleano
-    features = np.where(features == 1, True, False)
-
-    data_to_train = X_train_final.loc[:, features]
-    data_to_test = X_val_final.loc[:, features]
-    acuracia = classificador(data_to_train, data_to_test, y_train_final, y_val_final)
-
-    # unindo os dois objetivos em uma unica expressao, tornar um problema de minimizacao
-    return (1/acuracia) + lamda * qtd_features/qtd_features_maximas
-
-
-problem_dict = {
-    'fitness': fitness,
-    'lb': np.zeros(len(colunas)),
-    'ub': np.ones(len(colunas)),
-    'minmax': 'min',
-}
-
-epoch = 1000
-pop_size = 100
-pc = 0.9
-pm = 0.05
-selection = 'roulette'
-model1 = BaseGA(problem_dict, epoch, pop_size, pc, pm, crossover='multi_points', selection=selection, n_jobs=-1)
-best_position, best_fitness = model1.solve()
-print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
---
+++ PSO e ACO/PSO.py
@@ -0,0 +1,108 @@
+import numpy as np
+from mealpy.swarm_based.PSO import BasePSO
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+
+def main(qtd_features_maximas):
+    clf = RandomForestClassifier(criterion='entropy', n_estimators=100)
+    def classificador(data_to_train, data_to_test, y_train, y_test):
+        # instanciando o classificador
+        clf.fit(data_to_train, y_train)
+        y_chapeu = clf.predict(data_to_test)
+
+        return accuracy_score(y_test, y_chapeu)
+
+    def fitness(features):
+        # agora a quantidade de features sempre será fixa
+        # precisamos lidar com valores repetidos
+
+        if features.shape[0] - np.unique(features).shape[0] != 0:
+            return 10000
+        # precisamos de valores inteiros pois são sempre os indices das features a considerar
+        features = np.round(features).astype(np.int32).astype(str)
+
+        data_to_train = X_train_final.loc[:, features]
+        data_to_test = X_val_final.loc[:, features]
+
+        acuracia = classificador(data_to_train, data_to_test, y_train_final, y_val_final)
+
+        # unindo os dois objetivos em uma unica expressao, tornar um problema de minimizacao
+        return 1/acuracia
+
+    problem_dict = {
+        'fit_func': fitness,
+        'lb': 0,
+        'ub': 194999,
+        'minmax': 'min',
+        'save_population': False,
+        'n_dims': qtd_features_maximas,
+        'verbose': 0,
+    }
+
+    epoch = 10000
+    pop_size = 100
+    c1 = 2.05
+    c2 = 2.05
+    w_min = 0.4
+    w_max = 1.5
+
+    model1 = BasePSO(problem_dict, epoch, pop_size,  c1, c2, w_min, w_max)
+    best_position, best_fitness = model1.solve()
+    print(f"SolutionPSO {qtd_features_maximas}: {best_position}, Fitness: {best_fitness}")
+
+    ## Salvando as features encontradas em um arquivo txt
+    with open(f"../Features/PSO_features_{qtd_features_maximas}_{best_fitness:.2f}.txt", "w") as f:
+        for i in best_position:
+            f.write(str(i) + '\n')
+
+    # a partir desse ponto gera-se os dados de teste para submissao no kaggle
+    best_position.sort()
+    features_finais = np.round(best_position).astype(np.int32).astype(str)
+    val = pd.read_csv("../Dados/test.csv", dtype="float64")
+    beer_id = val.beer_id.astype(np.int32)
+    data_to_test = val.loc[:, features_finais]
+    data_to_test.replace([np.inf, -np.inf], np.nan, inplace=True)
+    data_to_test.fillna(data_to_test.mean(), inplace=True)
+
+    # classificando os dados de teste
+    # treinando nos dados
+    clf = RandomForestClassifier(criterion='entropy', n_estimators=100)
+    clf.fit(X_train_final.loc[:, features_finais], y_train_final)
+    predictions = clf.predict(data_to_test.astype("float32"))
+    submission = pd.DataFrame(np.stack((beer_id, predictions), axis=1),
+                              columns=["beer_id", "target"]).astype(dtype={"beer_id":"int32", "target":"float64"})
+    submission.to_csv(f"../Submissions/SubmissionPSO{qtd_features_maximas}.csv", index=False)
+
+
+if __name__ == '__main__':
+    import threading
+    import logging
+
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    # após o pré-processamento, essas devem ser as variaveis que recebem os dados
+    X_train_final = pd.read_csv('../Dados/DataTrain.csv', dtype="float32")
+    y_train_final = X_train_final['target']
+
+    X_val_final = pd.read_csv('../Dados/DataVal.csv', dtype="float32")
+    y_val_final = X_val_final['target']
+
+    colunas = X_train_final.columns
+
+    qtd_features = [150, 200, 250, 300]
+
+    threads = list()
+    for index in range(len(qtd_features)):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=main, args=(qtd_features[index],))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
